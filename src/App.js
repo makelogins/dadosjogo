84b0800fab5157fd936fca919f29816acb74155e
makephone <t.decarvalhosilva@gmail.com>
2021-06-10 17:59:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useRef , useState } from 'react'
 function App() {
+  const entrada = useRef()
+  const [numer, setNumero] = useState(null)
+function j(){
+  const r = Math.floor(Math.random() * 6) + 1;
+  analisar(entrada.current.value,r)
+}
+
+function analisar(a,b){
+ if(a/b===1){
+  setNumero("Você Ganhou !");
+ }else{
+  setNumero("Você Perdeu ! Número Sorteado = "+b);
+ }
+
+
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Digite um número entre 1 e 6</h2>  
+      <input type='text' ref={entrada} ></input>
+       <button onClick={j}>jogar</button>
+       <h2>{numer}</h2>
     </div>
   );
 }
 
 export default App;
+
